673afb036010954e257c48984781a1a282a00146
JuliSchegtel <julieta.schegtel@gmail.com>
2022-09-19 21:46:56 -0300

    actualizando app

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,36 +9,54 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
 		const product = products.find(product => product.id === +req.params.id);
 		return res.render('detail', { 
-			product
+			product,
+			toThousand
 		})
-		// Do the magic
 
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		const product = products.find(product => product.id === +req.params.id);
+		return res.render('product-edit-form',{
+			product
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		const {name,price,category,discount,description} = req.body;
+
+		const productModify = products.map(product => {
+			if(product.id === +req.params.id){
+				return {
+					...product,
+					name : name.trim(),
+					price : +price,
+					category,
+					discount : +discount,
+					description : description.trim()
+				}
+			}
+			return product
+		})
+
+		fs.writeFileSync(path.join(__dirname, '..', 'data', 'productsDataBase.json'),JSON.stringify(productModify), 'utf-8')
+		return res.redirect('/')
 	},
 
 	// Delete - Delete one product from DB
